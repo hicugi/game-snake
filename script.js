4a1678aebcc231552162bc0a7b0b0d688c76fb34
Mag <hicugi@gmail.com>
2025-01-08 17:48:30 +0500

    feat: init

---
+++ script.js
@@ -0,0 +1,14 @@
+const game = document.querySelector("#game");
+
+(() => {
+  const BOARD_SIZE = 64; // pixels
+
+  for (let r = 0; r < BOARD_SIZE; r++) {
+    const tr = document.createElement("TR");
+    for (let c = 0; c < BOARD_SIZE; c++) {
+      const td = document.createElement("TD");
+      tr.appendChild(td);
+    }
+    game.appendChild(tr);
+  }
+})();
